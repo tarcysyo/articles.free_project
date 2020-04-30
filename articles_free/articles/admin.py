a12bf1d8903570b2ee0d9ec71d7ab1b39f699561
tarcysyo <tarcisius3@yahoo.com.br>
2020-04-30 20:06:41 -0300

    Ajuste na modelagem de dados

--- articles_free/articles/admin.py
+++ articles_free/articles/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from articles_free.articles.models import Category, Article
+from articles_free.articles.models import Category, Article, Author
+
 
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'publication_date', 'category']
     search_fields = ['title', 'slug']
-    prepopulated_fields = {'slug': ('title', 'author')}
+    prepopulated_fields = {'slug': ('title', 'abstract')}
 
 admin.site.register(Category)
+admin.site.register(Author)
 admin.site.register(Article, ArticleAdmin)
--- articles_free/articles/models.py
+++ articles_free/articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField('Nome', max_length=100, blank=False, null=False)
@@ -12,12 +13,26 @@ class Category(models.Model):
         verbose_name_plural = 'Categorias'
         ordering = ['name']
 
+
+class Author(models.Model):
+    name = models.CharField('Nome', max_length=50, blank=False, null=False)
+    surname = models.CharField('Sobrenome', max_length=200, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+        ordering = ['name']
+
+
 class Article(models.Model):
     title = models.CharField('Título', max_length=255, blank=False, null=False)
-    author = models.CharField('Autor', max_length=255, blank=False, null=False)
-    slug = models.SlugField('Atalho', unique=True, max_length=30)
+    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING, related_name='authors', verbose_name="Autor")
     abstract = models.TextField('Resumo', blank=False, null=False)
-    publication_date = models.DateTimeField('Publicado em', auto_now_add=True)
+    slug = models.SlugField('Atalho', unique=True, max_length=50)
+    publication_date = models.DateField('Publicado em', auto_now_add=True)
     content = models.FileField('Artigo', upload_to='articles/files', blank=False, null=False)
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, related_name='articles', verbose_name="Categoria")
 
